974aca0ba1d05cbba6ddeafe86f9ad1372740a31
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-18 17:17:06 -0600

    malloc attempt with bad free call

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,15 @@
 */
 char *string_dup(char *src)
 {
-
+  char *copy = malloc(strlen(src));
+  int i = 0;
+  while (src[i] != '\0')
+  {
+    copy[i] = src[i];
+    i++;
+  }
+  copy[i] = '\0';
+  return copy;
 }
 
 /*
@@ -24,7 +32,12 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
+  char *char_dest = (char *)dest;
+  char *char_src = (char *)src;
+  for (int i = 0; i < n; i++)
+  {
+    char_dest[i] = char_src[i];
+  }
 }
 
 /*
@@ -40,48 +53,70 @@ void mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
+  printf("ptr %c, old_size %d, new_size %d\n", *(char *)ptr, old_size, new_size);
+  char *char_ptr = (char *)ptr;
+  if (old_size > new_size)
+  {
+    // printf("bad pointer: %c at %p\n", *(char_ptr + new_size), (char_ptr + new_size));
+    free((void *)(char_ptr + new_size));
+    return ptr;
+  }
+  else if (old_size < new_size)
+  {
+    char *new_ptr = malloc(new_size);
+    for (int i = 0; i < new_size; i++)
+    {
+      new_ptr[i] = char_ptr[i];
+    }
+    free(ptr);
+    return (void *)new_ptr;
+  }
+  return ptr;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *s = "Some string to duplicate.";
-    char *dup = string_dup(s);
+  char *s = "Some string to duplicate.";
+  char *dup = string_dup(s);
 
-    printf("Duplicated string: %s\n", dup);
+  printf("Duplicated string: %s\n", dup);
 
-    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-    int *target = malloc(n * sizeof(int));
-    
-    mem_copy(target, numbers, n * sizeof(int));
+  int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+  int n = sizeof(numbers) / sizeof(numbers[0]);
+  int *target = malloc(n * sizeof(int));
 
-    printf("Copied array: ");
+  mem_copy(target, numbers, n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", target[i]);
-    }
+  printf("Copied array: ");
 
-    printf("\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ", target[i]);
+  }
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
-    
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+  printf("\n");
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+  char *url = string_dup("http://lambdaschool.com");
+  char *path = string_dup("/students/");
+  int url_length = string_length(url);
+  int path_length = string_length(path);
 
-    printf("Full path string: %s\n", new_url);
+  int new_length = url_length - 1 + path_length;
+  char *new_url = resize_memory(url, url_length, new_length);
+  char *p = new_url + url_length;
 
-    return 0;
+  while (*path != '\0')
+  {
+    *p = *path;
+    p++;
+    path++;
+  }
+
+  printf("Full path string: %s\n", new_url);
+
+  resize_memory(new_url, new_length, url_length);
+
+  return 0;
 }
 #endif
