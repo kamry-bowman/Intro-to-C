e6f576b5f977a837938d860df9290b16961a493f
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-19 15:20:11 -0600

    passing quicksort test

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lib.h" 
+#include "lib.h"
 
 /*
     Implement the Quicksort algorithm. You'll likely want to re-use the
@@ -15,24 +15,48 @@
     
     Do not just use the `qsort` function from the standard library.
 */
+int partition(int *arr, int low, int high)
+{
+  int pivot = arr[low];
+  int pivot_target = high;
+  int right_pointer = high;
+  while (right_pointer > low)
+  {
+    if (arr[right_pointer] > pivot)
+    {
+      swap(arr + pivot_target, arr + right_pointer);
+      pivot_target--;
+    }
+    right_pointer--;
+  }
+  swap(arr + pivot_target, arr + low);
+  return pivot_target;
+}
+
 void quicksort(int *arr, int low, int high)
 {
-
+  if (low < high)
+  {
+    int middle = partition(arr, low, high);
+    quicksort(arr, low, middle - 1);
+    quicksort(arr, middle + 1, high);
+  }
 }
 
 #ifndef TESTING
 int main(void)
 {
-    int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(arr1) / sizeof(arr1[0]);
-    quicksort(arr1, 0, n-1);
+  int arr1[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+  int n = sizeof(arr1) / sizeof(arr1[0]);
+  quicksort(arr1, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr1[i]);
-    }
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ", arr1[i]);
+  }
 
-    printf("\n");
+  printf("\n");
 
-    return 0;
+  return 0;
 }
 #endif
